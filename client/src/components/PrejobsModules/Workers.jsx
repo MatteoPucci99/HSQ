c8c0bcb519e0bad70081f8331228f5587a926276
Matteo Pucci <136197193+MatteoPucci99@users.noreply.github.com>
2024-02-17 12:46:36 +0100

    Fix some styling on workers

--- client/src/components/PrejobsModules/Workers.jsx
+++ client/src/components/PrejobsModules/Workers.jsx
@@ -47,12 +47,12 @@ const Workers = (props)=>{
 
     return (
         <>
-            <Row className="mt-5">
+            <Row className="mt-5 row-cols-1">
                 <h2 className="text-center mb-5">Presenze lavoratori</h2>
                 <Col className="text-center">
-                    <TextField label="Nome lavoratore" name="workers" variant="outlined" value={worker.name_surname} onChange={(e)=>{setWorker(prevState=>({...prevState,   name_surname: e.target.value}))}} className="inputWidth"/>
+                    <TextField label="Nome e cognome" name="workers" variant="outlined" value={worker.name_surname} onChange={(e)=>{setWorker(prevState=>({...prevState,   name_surname: e.target.value}))}} className="inputWidth"/>
                 </Col>
-                <Col className="border p-2">
+                <Col className="border p-2 text-center mt-4">
                     <SignatureCanvas penColor='black' canvasProps={{ height: 200, className: 'myCanvas'}} ref={signatureRef} onEnd={handleSave}/>
                     <Button onClick={handleClear} variant="outline-danger">Cancella</Button>                 
                 </Col>
